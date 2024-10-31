7665a89bc024234ce51532939a8992317703f866
augi07 <102184778+augi07@users.noreply.github.com>
2024-10-31 22:09:48 +0100

    3b

--- server/api.js
+++ server/api.js
@@ -1,8 +1,15 @@
+const { body, validationResult} = require('express-validator');
+
 const initializeAPI = async (app) => {
-  app.post("/api/login", login);
+  app.post("/api/login", body('username').notEmpty().isEmail(), body('password').notEmpty().isLength({ min: 10 }), login);
 };
 
 const login = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   const { username, password } = req.body;
 
   const answer = `
